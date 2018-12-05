4d3df4762271a28ad6c0cb553aef1de7ce053c7d
quillasp <saralfddin93@yahoo.fr>
2018-12-05 13:24:31 +0100

    gestion des nombres romains terminées. Tout de même effectuer des contrôles.

--- main.cpp
+++ main.cpp
@@ -4,69 +4,81 @@
 using namespace std;
 
 string valeurRomaine(int unit, int multiple);
+char romain(int value);
 
 int main() {
 	int chiffre;
-	cin >> chiffre;
 
-	int nbDigits = (int)(log10(chiffre)) + 1;
-	string romanNumber;
+	while(cin >> chiffre) {
 
-	for (int i = nbDigits - 1; i >= 0; --i) {
-		int multiple = (int)pow(10, i);
-		int unit = (chiffre / multiple) * multiple;
-		romanNumber += valeurRomaine(unit, multiple);
-		chiffre = chiffre % multiple;
+		int nbDigits = (int) (log10(chiffre)) + 1;
+		string romanNumber;
+
+		for (int i = nbDigits - 1; i >= 0; --i) {
+			int multiple = (int) pow(10, i);
+			int unit = (chiffre / multiple) * multiple;
+			if (unit != 0 && i + 1 != 0)
+				romanNumber += valeurRomaine(unit, multiple);
+			chiffre = chiffre % multiple;
+		}
+		cout << romanNumber << endl;
 	}
-	cout << romanNumber << endl;
 	return 0;
 }
 
 string valeurRomaine(int unit, int multiple) {
 
+	if (unit < 1 || unit > 4999)
+		return "Entree invalide";
+
 	size_t mod = (size_t)(unit / multiple);
 	if (unit > 1000)
-		return string(mod, 'M');
+		return string(mod, romain(multiple));
 
-	else if (unit > 500) {
-		if (unit < 900)
-			return 'D' + string(mod, 'C');
+	else if (unit == 9 * multiple || unit == 4 * multiple)
+		return string(1, romain(multiple)) + string(1,romain(unit + multiple));
+
+	else if (unit >= multiple && unit < 4 * multiple)
+		return string(mod, romain(multiple));
+
+	else if (unit >= 5 * multiple && unit < 9 * multiple)
+		return romain(5 * multiple) + string(mod - 5, romain(multiple));
+
+	/*else if (unit > 5 * multiple) {
+		if (unit < 9 * multiple)
+			return romain(5 * multiple) + string(mod, romain(multiple));
 		else
-			return "CM";
+			return string(1, romain(multiple)) + string(1,romain(unit + multiple));
 	}
 
-	else if (unit > 100) {
-		if (unit < 400)
-			return string(mod, 'C');
+	else if (unit > 1 * multiple) {
+		if (unit < 4 * multiple)
+			return string(mod, romain(multiple));
 		else
-			return "CD";
-	}
+			return string(1, romain(multiple)) + string(1,romain(unit + multiple));
+	}*/
 
-	else if (unit > 50) {
-		if (unit < 90)
-			return 'L' + string(mod, 'X');
-		else
-			return "XC";
-	}
+	return "0";
+}
 
-	else if (unit > 10) {
-		if (unit < 40)
-			return string(mod, 'X');
-		else
-			return "XL";
-	}
+char romain(int value) {
 
-	else if (unit > 5) {
-		if (unit < 9)
-			return 'V' + string(mod, 'I');
-		else
-			return "IX";
-	}
-
-	else if (unit > 1) {
-		if (unit < 4)
-			return string(mod, 'I');
-		else
-			return "IV";
+	switch (value) {
+		case 1000 :
+			return 'M';
+		case 500 :
+			return 'D';
+		case 100 :
+			return 'C';
+		case 50 :
+			return 'L';
+		case 10 :
+			return 'X';
+		case 5 :
+			return 'V';
+		case 1 :
+			return 'I';
+		default:
+			return (char) 0;
 	}
 }
