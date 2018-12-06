27094ca82c6bdfd8d916315fe6155f92fa0c8898
ilias <ilias.goujgali@heig-vd.ch>
2018-12-06 11:01:02 +0100

    ajout de la fonction de conversion d'un nombre romain en nombre entier

--- main.cpp
+++ main.cpp
@@ -4,29 +4,47 @@
 using namespace std;
 
 string valeurRomaine(int unit, int multiple);
-
-char romain(int value);
+int romanChar2Int(char romanChar);
+int romanToInt(string romanNumber);
+char int2RomanChar(int value);
 
 int main() {
     int chiffre;
-
-    while (cin >> chiffre) {
-
-        int nbDigits = (int) (log10(chiffre)) + 1;
-        string romanNumber;
-
-        for (int i = nbDigits - 1; i >= 0; --i) {
-            int multiple = (int) pow(10, i);
-            int unit = (chiffre / multiple) * multiple;
-            if (unit != 0 && i + 1 != 0)
-                romanNumber += valeurRomaine(unit, multiple);
-            chiffre = chiffre % multiple;
-        }
-        cout << romanNumber << endl;
-    }
+//    // Int to roman string
+//    while (cin >> chiffre) {
+//
+//        int nbDigits = (int) (log10(chiffre)) + 1;
+//        string romanNumber;
+//
+//        for (int i = nbDigits - 1; i >= 0; --i) {
+//            int multiple = (int) pow(10, i);
+//            int unit = (chiffre / multiple) * multiple;
+//            if (unit != 0 && i + 1 != 0)
+//                romanNumber += valeurRomaine(unit, multiple);
+//            chiffre = chiffre % multiple;
+//        }
+//        cout << romanNumber << endl;
+//    }
+    //Roman string to int
+    string romanString;
+    cin >> romanString;
+    cout << romanToInt(romanString);
     return 0;
 }
-
+int romanToInt(string romanNumber)
+{
+    int result= 0;
+    for(size_t i = 0; i < romanNumber.length(); ++i)
+    {
+        int value = romanChar2Int(romanNumber[i]);
+        if(i < romanNumber.length() - 1 &&  value < romanChar2Int(romanNumber[i + 1]) )
+        {
+            value *= (-1);
+        }
+        result += value;
+    }
+    return result;
+}
 string valeurRomaine(int unit, int multiple) {
 
     if (unit < 1 || unit > 4999)
@@ -35,22 +53,22 @@ string valeurRomaine(int unit, int multiple) {
     size_t mod = (size_t) (unit / multiple);
     // thousand, 3000 => MMM, 4123 => MMMM
     if (unit > 1000)
-        return string(mod, romain(multiple));
+        return string(mod, int2RomanChar(multiple));
 
-        // case when it has to subtract
+    // case when it has to subtract
     else if (unit == 9 * multiple || unit == 4 * multiple)
-        return string(1, romain(multiple)) + string(1, romain(unit + multiple));
-        // case when it has to add
+        return string(1, int2RomanChar(multiple)) + string(1, int2RomanChar(unit + multiple));
+    // case when it has to add
     else if (unit >= multiple && unit < 4 * multiple)
-        return string(mod, romain(multiple));
-        // case when it has to add to a 5 * 10^n multiple (5 = V, 50 = L, 500 = D), 5 => V,6=> VI, 70 => LII , 800 => DCCC
+        return string(mod, int2RomanChar(multiple));
+    // case when it has to add to a 5 * 10^n multiple (5 = V, 50 = L, 500 = D), 5 => V,6=> VI, 70 => LII , 800 => DCCC
     else if (unit >= 5 * multiple && unit < 9 * multiple)
-        return romain(5 * multiple) + string(mod - 5, romain(multiple));
+        return int2RomanChar(5 * multiple) + string(mod - 5, int2RomanChar(multiple));
 
     return "0";
 }
 
-char romain(int value) {
+char int2RomanChar(int value) {
 
     switch (value) {
         case 1000 :
@@ -70,4 +88,26 @@ char romain(int value) {
         default:
             return (char) 0;
     }
+}
+int romanChar2Int(char romanChar)
+{
+    switch (romanChar) {
+        case 'M' :
+            return 1000;
+        case 'D' :
+            return  500;
+        case 'C' :
+            return 100;
+        case 'L' :
+            return 50;
+        case 'X' :
+            return 10;
+        case 'V' :
+            return 5;
+        case 'I' :
+            return 1;
+        default:
+            return (char) 0;
+    }
+
 }
